210cc6ffee291b1dd46d3ddceae30da679b2998b
Ranjan Kumar <ranjan.rk@gmail.com>
2014-05-19 18:38:50 +0530

    Added service monitor using AOP

---
+++ src/main/java/com/restfiddle/aop/ServiceMonitor.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2014 Ranjan Kumar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.restfiddle.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ServiceMonitor {
+    Logger logger = LoggerFactory.getLogger(ServiceMonitor.class);
+
+    @AfterReturning("execution(* com.restfiddle.controller.rest.*.*(..))")
+    public void logServiceAccess(JoinPoint joinPoint) {
+	logger.info("Completed: " + joinPoint);
+    }
+}
