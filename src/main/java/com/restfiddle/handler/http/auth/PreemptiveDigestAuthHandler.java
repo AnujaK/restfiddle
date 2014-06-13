5336250b6be55e48ade979b65aefc0e414198541
Ranjan Kumar <ranjan.rk@gmail.com>
2014-06-13 20:28:02 +0530

    fixed formatting and license

--- src/main/java/com/restfiddle/handler/http/auth/PreemptiveDigestAuthHandler.java
+++ src/main/java/com/restfiddle/handler/http/auth/PreemptiveDigestAuthHandler.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2014 Sandesh Deshmane
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.restfiddle.handler.http.auth;
 
 import org.apache.http.auth.AuthScope;
@@ -25,20 +9,16 @@ import org.apache.http.impl.client.HttpClients;
 
 public class PreemptiveDigestAuthHandler {
 
-	public PreemptiveDigestAuthHandler() {
+    public PreemptiveDigestAuthHandler() {
 
-	}
+    }
 
-	public CloseableHttpClient PreemptiveDigestAuth(String userName,
-			String password) {
-		CredentialsProvider credsProvider = new BasicCredentialsProvider();
-		credsProvider.setCredentials(new AuthScope(AuthScope.ANY_HOST,
-				AuthScope.ANY_PORT, AuthScope.ANY_REALM),
-				new UsernamePasswordCredentials(userName, password));
-		CloseableHttpClient httpclient = HttpClients.custom()
-				.setDefaultCredentialsProvider(credsProvider).build();
-		return httpclient;
+    public CloseableHttpClient PreemptiveDigestAuth(String userName, String password) {
+	CredentialsProvider credsProvider = new BasicCredentialsProvider();
+	credsProvider.setCredentials(new AuthScope(AuthScope.ANY_HOST, AuthScope.ANY_PORT, AuthScope.ANY_REALM), new UsernamePasswordCredentials(
+		userName, password));
+	CloseableHttpClient httpclient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+	return httpclient;
 
-	}
-
+    }
 }
