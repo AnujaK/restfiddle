bb5d5b2c26fdc3cdadd83caa31328ea9ca45cc6e
Ranjan Kumar <ranjan.rk@gmail.com>
2014-06-25 22:49:51 +0530

    Tags can exist in a workspace...and can not be copied to another workspace

---
+++ src/main/java/com/restfiddle/entity/Tag.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014 Ranjan Kumar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.restfiddle.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Tag extends NamedEntity {
+    private static final long serialVersionUID = 1L;
+
+    @OneToOne
+    private Color color;
+
+    @ManyToOne
+    private Workspace workspace;
+
+    public Color getColor() {
+	return color;
+    }
+
+    public void setColor(Color color) {
+	this.color = color;
+    }
+
+    public Workspace getWorkspace() {
+	return workspace;
+    }
+
+    public void setWorkspace(Workspace workspace) {
+	this.workspace = workspace;
+    }
+
+}
