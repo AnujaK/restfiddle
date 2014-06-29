da353b169e7a0fedb507b4238241b3c9f6bd560f
Ranjan Kumar <ranjan.rk@gmail.com>
2014-06-29 23:03:53 +0530

    added placeholders for role and permission controllers

---
+++ src/main/java/com/restfiddle/controller/rest/RoleController.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2014 Ranjan Kumar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.restfiddle.controller.rest;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.restfiddle.dao.RoleRepository;
+
+@RestController
+@EnableAutoConfiguration
+@ComponentScan
+@Transactional
+public class RoleController {
+    Logger logger = LoggerFactory.getLogger(RoleController.class);
+
+    @Resource
+    private RoleRepository roleRepository;
+
+}
