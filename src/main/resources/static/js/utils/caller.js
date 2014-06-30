f2a234da0816dacd3586737dec38efda01b8fe0e
Hemant Sachdeva <hemant.sachdeva04@gmail.com>
2014-06-30 15:11:27 +0530

    Hiding Modal after workspace got saved.

--- src/main/resources/static/js/utils/caller.js
+++ src/main/resources/static/js/utils/caller.js
@@ -26,10 +26,15 @@ $(function() {
     }
 
     function saveWorkspace() {
-	var newWorkspace = new app.Workspace({
-	    name : $("#workspaceTextField").val(),
-	});
-	newWorkspace.save();
+		var newWorkspace = new app.Workspace({
+		    name : $("#workspaceTextField").val(),
+		});
+		newWorkspace.save(null, {success : function(){
+			$("#workspaceModal").modal("hide");
+		}, error : function(e){
+			$("#workspaceModal").modal("hide");
+			alert('Some unexpected error occured Please try later.');
+		}});
     }
 
     $("#run").bind("click", send);
