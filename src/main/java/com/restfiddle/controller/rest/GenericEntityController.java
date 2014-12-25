911f9fffa13e4ee44db8f4e1970483b2feff18f2
AnujaK <a.for.anuja@gmail.com>
2014-12-25 16:01:16 +0530

    Generic entity--WIP

---
+++ src/main/java/com/restfiddle/controller/rest/GenericEntityController.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2014 Ranjan Kumar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing entities and
+ * limitations under the License.
+ */
+package com.restfiddle.controller.rest;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.restfiddle.dao.GenericEntityRepository;
+import com.restfiddle.dto.GenericEntityDTO;
+import com.restfiddle.dto.GenericEntityFieldDTO;
+import com.restfiddle.entity.GenericEntity;
+import com.restfiddle.entity.GenericEntityField;
+
+@RestController
+@EnableAutoConfiguration
+@ComponentScan
+@Transactional
+public class GenericEntityController {
+    Logger logger = LoggerFactory.getLogger(GenericEntityController.class);
+
+    @Resource
+    private GenericEntityRepository genericEntityRepository;
+
+    @RequestMapping(value = "/api/entities", method = RequestMethod.POST, headers = "Accept=application/json")
+    public @ResponseBody
+    GenericEntity create(@RequestBody GenericEntityDTO genericEntityDTO) {
+	logger.debug("Creating a new entity with information: " + genericEntityDTO);
+
+	GenericEntity entity = new GenericEntity();
+	entity.setName(genericEntityDTO.getName());
+	entity.setDescription(genericEntityDTO.getDescription());
+
+	List<GenericEntityFieldDTO> fieldDTOs = genericEntityDTO.getFields();
+	if (fieldDTOs != null && !fieldDTOs.isEmpty()) {
+	    List<GenericEntityField> fields = new ArrayList<GenericEntityField>();
+	    GenericEntityField field = null;
+	    for (GenericEntityFieldDTO fieldDTO : fieldDTOs) {
+		field = new GenericEntityField();
+		field.setName(fieldDTO.getName());
+		field.setType(fieldDTO.getType());
+		fields.add(field);
+	    }
+	    entity.setFields(fields);
+	}
+
+	entity = genericEntityRepository.save(entity);
+	return entity;
+    }
+
+    @RequestMapping(value = "/api/entities/{id}", method = RequestMethod.DELETE, headers = "Accept=application/json")
+    public @ResponseBody
+    GenericEntity delete(@PathVariable("id") Long id) {
+	logger.debug("Deleting entity with id: " + id);
+
+	GenericEntity deleted = genericEntityRepository.findOne(id);
+
+	genericEntityRepository.delete(deleted);
+
+	return deleted;
+    }
+
+    @RequestMapping(value = "/api/entities", method = RequestMethod.GET)
+    public @ResponseBody
+    List<GenericEntity> findAll() {
+	logger.debug("Finding all entities");
+
+	return genericEntityRepository.findAll();
+    }
+
+    @RequestMapping(value = "/api/entities/{id}", method = RequestMethod.GET)
+    public @ResponseBody
+    GenericEntity findById(@PathVariable("id") Long id) {
+	logger.debug("Finding entity by id: " + id);
+
+	return genericEntityRepository.findOne(id);
+    }
+
+    @RequestMapping(value = "/api/entities/{id}", method = RequestMethod.PUT, headers = "Accept=application/json")
+    public @ResponseBody
+    GenericEntity update(@PathVariable("id") Long id, @RequestBody GenericEntityDTO updated) {
+	logger.debug("Updating entity with information: " + updated);
+
+	GenericEntity entity = genericEntityRepository.findOne(updated.getId());
+
+	entity.setName(updated.getName());
+	entity.setDescription(updated.getDescription());
+
+	return entity;
+    }
+}
---
+++ src/main/java/com/restfiddle/entity/GenericEntity.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2014 Ranjan Kumar
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.restfiddle.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+@Entity
+public class GenericEntity extends NamedEntity {
+    private static final long serialVersionUID = 1L;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "genericEntity")
+    @JsonManagedReference
+    private List<GenericEntityField> fields = new ArrayList<GenericEntityField>();
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "genericEntity")
+    @JsonManagedReference
+    private List<GenericEntityData> entityDataList = new ArrayList<GenericEntityData>();
+
+    public List<GenericEntityField> getFields() {
+	return fields;
+    }
+
+    public void setFields(List<GenericEntityField> fields) {
+	this.fields = fields;
+    }
+
+    public List<GenericEntityData> getEntityDataList() {
+	return entityDataList;
+    }
+
+    public void setEntityDataList(List<GenericEntityData> entityDataList) {
+	this.entityDataList = entityDataList;
+    }
+
+}
